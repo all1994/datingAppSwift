01ce807e59a1802718488adac3447063b4506edd
HG <HG@HGs-MacBook-Pro.local>
2017-02-17 18:08:26 -0800

    initial commit

--- r3d3/AppDelegate.swift
+++ r3d3/AppDelegate.swift
@@ -7,18 +7,46 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+
+    
     var window: UIWindow?
 
+    
+    override init() {
+        super.init()
+        FIRApp.configure()
+    }
+    
+    func logUser(){
+        
+        if FIRAuth.auth()!.currentUser != nil {
+            
+            let tabBar = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBar") as! TabBarViewController
+            self.window?.rootViewController = tabBar
+        }
+        
+        
+    }
+    
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        logUser()
         // Override point for customization after application launch.
         return true
     }
 
+    
+    
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
